60afddee1614a22371488f0fa5fbf51a5e7ca12c
Abdelrahman-Gharieb <abdelrahmanghareb5@gmail.com>
2026-05-05 22:56:11 +0300

    Creats a Node.js Application

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    return 'Hello, ${name}!'
+}
+
+module.exports = greet; // Ensure Function Is exportable 
+// If run directly, print output (useful for debugging)
+
+if (require.main == module) {
+
+    console.log(greet("World"));
+}
---
+++ src/test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+#src/test.sh
+EXPECTED="Hello, Test!"
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
+
+if [[ "$OUTPUT" ==  "$EXPECTED"]]; then
+    echo "Tetst passed! "
+    exit 0
+else
+    echo "Test Failed! Expected '$EXPECTED' but got '$OUTPUT'"
+    exit 1
+fi
