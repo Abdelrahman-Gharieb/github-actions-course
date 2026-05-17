e5baf118a6922f47d1379b3e2456e9ed7502d8e0
Abdelrahman-Gharieb <abdelrahmanghareb5@gmail.com>
2026-05-17 22:12:08 +0300

    Test Changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ function greet(name) {
 module.exports = greet; // Ensure Function Is exportable 
 // If run directly, print output (useful for debugging)
 
-if (require.main == module) {
-
-    console.log(greet("World"));
-}
+//if (require.main == module) {
+//    console.log(greet("World"));
+//}
--- src/test.sh
+++ src/test.sh
@@ -5,7 +5,7 @@ EXPECTED="Hello, Test!"
 OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 
 if [[ "$OUTPUT" ==  "$EXPECTED" ]]; then
-    echo "Tetst passed! "
+    echo "Tetst passed!"
     exit 0
 else
     echo "Test Failed! Expected '$EXPECTED' but got '$OUTPUT'"
